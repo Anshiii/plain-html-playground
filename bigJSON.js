2a2d1af0dd339bd999d0b88952a732de38776b14
Eula Zeng <eula@theplant.jp>
2023-04-25 16:47:46 +0800

    add: bigsearch

---
+++ bigJSON.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+let sourceItem = {
+  id: 1,
+  name: randomString(16),
+};
+
+function randomString(e) {
+  e = e || 32;
+  var t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz",
+    a = t.length,
+    n = "";
+  for (i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
+  return n;
+}
+
+let bigJSON = { data: [] };
+
+for (let i = 0; i < 100000; i++) {
+  bigJSON.data.push({
+    id: i,
+    name: randomString(12),
+  });
+}
+
+const data = JSON.stringify(bigJSON);
+
+// write JSON string to a file
+fs.writeFile('big.json', data, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
+});
